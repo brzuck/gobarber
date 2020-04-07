08827a1091d1c6171f7182cf09d06348e98942b3
Bruno Barros <bruno@cubeti.com.br>
2020-04-07 16:56:41 -0300

    using multer to create file upload for avatar image

--- gobarber-server/src/routes.js
+++ gobarber-server/src/routes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
+import multer from 'multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
@@ -11,5 +16,9 @@ routes.post('/users', UserController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
