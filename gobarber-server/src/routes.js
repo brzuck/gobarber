21d91023ed92a126f8ddf834f59adf1d96f78db6
Bruno Barros <bruno@cubeti.com.br>
2020-04-07 17:25:51 -0300

    creating appointment

--- gobarber-server/src/routes.js
+++ gobarber-server/src/routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import multer from 'multer';
-import UserController from './app/controllers/UserController';
-import SessionController from './app/controllers/SessionController';
-import authMiddleware from './app/middlewares/auth';
-import multerConfig from './config/multer';
+import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
+import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -18,6 +19,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
