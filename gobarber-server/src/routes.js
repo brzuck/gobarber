f6277386f814a6b9a6d249f2ad041d6b9a40f00c
Bruno Barros <bruno@cubeti.com.br>
2020-04-08 19:16:39 -0300

    provider schedule list

--- gobarber-server/src/routes.js
+++ gobarber-server/src/routes.js
@@ -3,6 +3,7 @@ import multer from 'multer';
 import AppointmentController from './app/controllers/AppointmentController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import ScheduleController from './app/controllers/ScheduleController';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import authMiddleware from './app/middlewares/auth';
@@ -22,6 +23,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
