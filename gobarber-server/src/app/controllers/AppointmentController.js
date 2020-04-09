9c7d36139c236bb8aff98cc10d677210cbdcf0db
Bruno Barros <bruno@cubeti.com.br>
2020-04-09 17:36:37 -0300

    background job implementation using redis queue

--- gobarber-server/src/app/controllers/AppointmentController.js
+++ gobarber-server/src/app/controllers/AppointmentController.js
@@ -1,7 +1,8 @@
-import { subHours, format, isBefore, parseISO, startOfHour } from 'date-fns';
+import { format, isBefore, parseISO, startOfHour, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt-BR';
 import * as Yup from 'yup';
-import Mail from '../../lib/Mail';
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 import Appointment from '../models/Appointment';
 import File from '../models/File';
 import User from '../models/User';
@@ -139,17 +140,8 @@ class AppointmentController {
 
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      template: 'cancellation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "dd 'de' MMMM', às' H:mm'h'", {
-          locale: pt,
-        }),
-      },
+    Queue.add(CancellationMail.key, {
+      appointment,
     });
 
     return res.json(appointment);
