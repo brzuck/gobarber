4033e4338274a2d34a5f486fe2f2e4842f4cc3c7
Bruno Barros <bruno@cubeti.com.br>
2020-04-09 16:18:02 -0300

    creating appointment canceling route

--- gobarber-server/src/app/controllers/AppointmentController.js
+++ gobarber-server/src/app/controllers/AppointmentController.js
@@ -1,4 +1,4 @@
-import { format, isBefore, parseISO, startOfHour } from 'date-fns';
+import { subHours, format, isBefore, parseISO, startOfHour } from 'date-fns';
 import pt from 'date-fns/locale/pt-BR';
 import * as Yup from 'yup';
 import Appointment from '../models/Appointment';
@@ -100,6 +100,30 @@ class AppointmentController {
       content: `Novo agendamento de ${user.name} para dia ${formattedDate}`,
       user: provider_id,
     });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if (appointment.user_id !== req.userId) {
+      return res.json(401).json({
+        error: "You don't have permission to cancel this appointment",
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res
+        .status(401)
+        .json({ error: 'You can only cancel appointments 2 hours in advance' });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
 
     return res.json(appointment);
   }
