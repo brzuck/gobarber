d3069396530e5f71dd550a485b1362332ecc209f
Bruno Barros <bruno@cubeti.com.br>
2020-04-18 18:18:55 -0300

    profile page update with file upload

--- gobarber-web/src/store/modules/user/sagas.js
+++ gobarber-web/src/store/modules/user/sagas.js
@@ -1,3 +1,28 @@
-import { all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+import { all, call, put, takeLatest } from 'redux-saga/effects';
+import api from '~/services/api';
+import { updateProfileFailure, updateProfileSucess } from './actions';
 
-export default all([]);
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, avatar_id, ...rest } = payload.data;
+
+    const profile = {
+      name,
+      email,
+      avatar_id,
+      ...(rest.oldPassword ? rest : {}),
+    };
+
+    const response = yield call(api.put, 'users', profile);
+
+    toast.success('Perfil atualizado com sucesso!');
+
+    yield put(updateProfileSucess(response.data));
+  } catch (err) {
+    toast.error('Erro ao atualizar perfil, confira seus dados!');
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
