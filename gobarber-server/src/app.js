91ded1fea38da2558951a73df90cc0bdef6232d2
Bruno Barros <bruno@cubeti.com.br>
2020-04-17 23:52:47 -0300

    enabling cors and adding new props to some apis

--- gobarber-server/src/app.js
+++ gobarber-server/src/app.js
@@ -1,4 +1,5 @@
 import * as Sentry from '@sentry/node';
+import cors from 'cors';
 import 'dotenv/config';
 import express from 'express';
 import 'express-async-errors';
@@ -21,6 +22,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
