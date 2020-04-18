6e445d6af491f5ccca40ae9cf347b28131affe4d
Bruno Barros <bruno@cubeti.com.br>
2020-04-18 00:40:28 -0300

    configuring routes and reactotron

---
+++ gobarber-web/src/pages/_layouts/auth/index.js
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Wrapper } from './styles';
+
+export default function AuthLayout({ children }) {
+  return <Wrapper>{children}</Wrapper>;
+}
+
+AuthLayout.propTypes = {
+  children: PropTypes.element.isRequired,
+};
