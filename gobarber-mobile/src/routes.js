c3d1d9ec350f34122f5bc93dd81b9fc0b87d69f3
Bruno Barros <bruno@cubeti.com.br>
2020-04-21 14:55:25 -0300

    initial route

--- gobarber-mobile/src/routes.js
+++ gobarber-mobile/src/routes.js
@@ -1,29 +1,49 @@
-import * as React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Dashboard from './pages/Dashboard';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const Sign = () => {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="SignIn" component={SignIn} />
+      <Stack.Screen name="SignUp" component={SignUp} />
+    </Stack.Navigator>
+  );
+};
+
+const App = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Dashboard" component={Dashboard} />
+    </Tab.Navigator>
+  );
+};
 
 export default function Routes() {
+  const isSigned = useSelector((state) => state.auth.signed);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="SignIn"
-          component={SignIn}
-          options={{
-            headerShown: false,
-          }}
-        />
-        <Stack.Screen
-          name="SignUp"
-          component={SignUp}
-          options={{
-            headerShown: false,
-          }}
-        />
+      <Stack.Navigator
+        initialRouteName={isSigned ? 'App' : 'Sign'}
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Sign" component={Sign} />
+        <Stack.Screen name="App" component={App} />
       </Stack.Navigator>
     </NavigationContainer>
   );
